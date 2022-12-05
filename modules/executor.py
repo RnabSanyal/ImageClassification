5717167af62e6606bbfaea402a29771dc8b75343
Arnab Sanyal <rnv.san96@gmail.com>
2022-12-05 18:57:05 -0500

    feature extraction integration, subset training loop

--- modules/executor.py
+++ modules/executor.py
@@ -5,10 +5,16 @@ from pprint import pprint
 class Executor:
 
 
-    def __init__(self, data_source = 'facedata', feautureExtractor=None):
+    def __init__(self, data_source = 'facedata', feautureExtractor=None, algorithm=None, metric = 'acc'):
 
         self.data_source = data_source
         self.prepareData()
+        self.metric = metric
+        if feautureExtractor:
+            self.feautureExtractor = feautureExtractor
+        if algorithm:
+            self.algorithm = algorithm
+        self.execute()
 
 
     def prepareData(self):
@@ -43,6 +49,17 @@ class Executor:
                     self.Y_val = self.parseLabels(file_path)
                 else:
                     self.X_val = self.parseData(file_path, rows)
+        
+        if self.feautureExtractor:
+            
+            self.X_train = self.feautureExtractor.extract(self.X_train)
+            self.X_val = self.feautureExtractor.extract(self.X_val)
+            self.X_test = self.feautureExtractor.extract(self.X_test)
+        
+        # flatten data arrays for training
+        self.X_train = self.flatten(self.X_train)
+        self.X_val = self.flatten(self.X_val)
+        self.X_test = self.flatten(self.X_test)
 
 
     def parseData(self, file_path, rows):
@@ -68,3 +85,62 @@ class Executor:
         file_handle = open(file_path)
         lines = file_handle.readlines()
         return np.array([int(line[:-1]) for line in lines])
+
+
+    def execute(self):
+
+        # Run training with different subsets of the data, 10%, 20%, ..., 100%
+        # Randomly sample data for each subset 5 times 
+        # calcualte mean and std of accuracy on train, validation and test
+
+        if not self.algorithm:
+            raise Exception("Learning Algorithm not provided")
+
+        no_imgs, img_rows, img_cols = self.X_train.shape
+
+        self.train_mean_met = []
+        self.train_std_met = []
+        self.val_mean_met = []
+        self.val_std_met = []
+        self.test_mean_met = []
+        self.test_std_met = []
+
+        increment = int(no_imgs/10)
+
+        for i in range(1,11):
+
+            idx = np.random.randint(0, no_imgs, size=(increment*i))
+            train_met = []
+            val_met = []
+            test_met = []
+
+            for i in range(5):
+
+                X = self.X_train[idx, :]
+                Y = self.Y_train[idx, :]
+
+                self.algorithm.fit(X, Y)
+
+                train_met.append(self.calculate_metric(self.algorithm, X, Y, self.metric))
+                val_met.append(self.calculate_metric(self.algorithm, self.X_val, self.Y_val, self.metric))
+                test_met.append(self.calculate_metric(self.algorithm, self.X_test, self.Y_test, self.metric))
+            
+            self.train_mean_met.append(np.mean(train_met))
+            self.train_std_met.append(np.std(train_met))
+
+            self.val_mean_met.append(np.mean(val_met))
+            self.val_std_met.append(np.std(val_met))
+
+            self.test_mean_met.append(np.mean(test_met))
+            self.test_std_met.append(np.std(test_met))
+
+    
+    def calculate_metric(algorithm, X, Y, metric = 'acc'):
+        pass
+
+
+    def flatten(X):
+
+        # return flattened 2D matrix
+        no_imgs, img_rows, img_cols = X.shape
+        return X.reshape(no_imgs, img_rows * img_cols)
--- modules/feature.py
+++ modules/feature.py
@@ -17,9 +17,9 @@ class FeatureExtractor:
     # grid must divide the image dimensions 
 
 
-    def __int__(self, grid_structure = (4,4), round_off = False):
+    def __init__(self, grid_shape = (4,4), round_off = False):
             
-            self.grid_structure = grid_structure
+            self.grid_shape = grid_shape
             self.round_off = round_off
 
 
@@ -31,14 +31,14 @@ class FeatureExtractor:
         #    expected shape: (# images, rows, cols)
 
         no_imgs, img_rows, img_cols = X.shape
-        grid_rows, grid_cols = self.grid_structure
+        grid_rows, grid_cols = self.grid_shape
 
         if img_rows % grid_rows or img_cols % grid_cols:
             raise Exception("Grid size incompatible with image dimensions")
         
         # reshape x to run calculations on axes directly
         X_reshaped = \
-            X.reshape(no_imgs, img_rows/grid_rows, grid_rows, img_cols/grid_cols, grid_cols)
+            X.reshape(no_imgs, int(img_rows/grid_rows), grid_rows, int(img_cols/grid_cols), grid_cols)
         
         X_features = X_reshaped.sum(axis = (2,4)) / (grid_rows * grid_cols)
 
